4fced282b50b58dc027bb60309a35c73d5048489
DavidIfeOyat <153612673+DavidIfeOyat@users.noreply.github.com>
2024-08-16 00:10:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Array of image objects with titles
     const images = [
-        { src: 'img/draw1.jpg', alt: 'Drawing 1', title: 'Beautiful Drawing 1' },
-        { src: 'img/draw2.jpg', alt: 'Drawing 2', title: 'Beautiful Drawing 2' },
-        { src: 'img/draw3.jpg', alt: 'Drawing 3', title: 'Beautiful Drawing 3' },
-        { src: 'img/draw4.jpg', alt: 'Drawing 4', title: 'Beautiful Drawing 4' },
-        { src: 'img/draw5.jpg', alt: 'Drawing 5', title: 'Beautiful Drawing 5' },
-        { src: 'img/draw6.jpg', alt: 'Drawing 6', title: 'Beautiful Drawing 6' },
-        { src: 'img/draw7.jpg', alt: 'Drawing 7', title: 'Beautiful Drawing 7' },
-        { src: 'img/draw8.jpg', alt: 'Drawing 8', title: 'Beautiful Drawing 8' },
-        { src: 'img/draw9.jpg', alt: 'Drawing 9', title: 'Beautiful Drawing 9' },
-        { src: 'img/draw10.jpg', alt: 'Drawing 10', title: 'Beautiful Drawing 10' },
-        { src: 'img/draw11.jpg', alt: 'Drawing 11', title: 'Beautiful Drawing 11' },
-        { src: 'img/draw12.jpg', alt: 'Drawing 12', title: 'Beautiful Drawing 12' },
-        { src: 'img/draw13.jpg', alt: 'Drawing 13', title: 'Beautiful Drawing 13' },
-        { src: 'img/draw14.jpg', alt: 'Drawing 14', title: 'Beautiful Drawing 14' },
-        { src: 'img/draw15.jpg', alt: 'Drawing 15', title: 'Beautiful Drawing 15' },
-        { src: 'img/draw16.jpg', alt: 'Drawing 16', title: 'Beautiful Drawing 16' },
+        { src: 'img/draw1.jpg', alt: 'Drawing 1', title: 'Lost Samurai' },
+        { src: 'img/draw2.jpg', alt: 'Drawing 2', title: 'Karl Kopinski (Recreation) ' },
+        { src: 'img/draw3.jpg', alt: 'Drawing 3', title: 'Joestar' },
+        { src: 'img/draw4.jpg', alt: 'Drawing 4', title: 'Nightwing' },
+        { src: 'img/draw5.jpg', alt: 'Drawing 5', title: 'Selfie' },
+        { src: 'img/draw6.jpg', alt: 'Drawing 6', title: 'Miyamoto Musashi Praying' },
+        { src: 'img/draw7.jpg', alt: 'Drawing 7', title: 'Lady Potrait' },
+        { src: 'img/draw8.jpg', alt: 'Drawing 8', title: 'Takuna' },
+        { src: 'img/draw9.jpg', alt: 'Drawing 9', title: 'Leornado' },
+        { src: 'img/draw10.jpg', alt: 'Drawing 10', title: 'Hulk' },
+        { src: 'img/draw11.jpg', alt: 'Drawing 11', title: 'GhostRider' },
+        { src: 'img/draw12.jpg', alt: 'Drawing 12', title: 'Cold in the Sea' },
+        { src: 'img/draw13.jpg', alt: 'Drawing 13', title: 'Fitpic' },
+        { src: 'img/draw14.jpg', alt: 'Drawing 14', title: 'Batman V superman' },
+        { src: 'img/draw15.jpg', alt: 'Drawing 15', title: 'Lookbook' },
+        { src: 'img/draw16.jpg', alt: 'Drawing 16', title: 'Ginger Ebony' },
     ];
 
     // Loop through the array and create image elements
