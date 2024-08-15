4ea505a4affe7c314ac1be1301c5d26f7743aeac
DavidIfeOyat <153612673+DavidIfeOyat@users.noreply.github.com>
2024-08-16 00:02:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
         { src: 'img/draw7.jpg', alt: 'Drawing 7', title: 'Beautiful Drawing 7' },
         { src: 'img/draw8.jpg', alt: 'Drawing 8', title: 'Beautiful Drawing 8' },
         { src: 'img/draw9.jpg', alt: 'Drawing 9', title: 'Beautiful Drawing 9' },
-        { src: 'img/aboutme.jpg', alt: 'About Me', title: 'About the Artist' }
+        { src: 'img/draw10.jpg', alt: 'Drawing 10', title: 'Beautiful Drawing 10' },
+        { src: 'img/draw11.jpg', alt: 'Drawing 11', title: 'Beautiful Drawing 11' },
+        { src: 'img/draw12.jpg', alt: 'Drawing 12', title: 'Beautiful Drawing 12' },
+        { src: 'img/draw13.jpg', alt: 'Drawing 13', title: 'Beautiful Drawing 13' },
+        { src: 'img/draw14.jpg', alt: 'Drawing 14', title: 'Beautiful Drawing 14' },
+        { src: 'img/draw15.jpg', alt: 'Drawing 15', title: 'Beautiful Drawing 15' },
+        { src: 'img/draw16.jpg', alt: 'Drawing 16', title: 'Beautiful Drawing 16' },
     ];
 
     // Loop through the array and create image elements
